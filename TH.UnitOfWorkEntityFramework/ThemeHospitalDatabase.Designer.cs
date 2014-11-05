0c5f86f5c7a4d2544737638ae9972d00e2dd8d80
Jonny Booker <jonathan.booker@hotmail.co.uk>
2014-11-05 14:39:06 +0000

    Diagram line changes so they ain't wonky

--- TH.UnitOfWorkEntityFramework/ThemeHospitalDatabase.Designer.cs
+++ TH.UnitOfWorkEntityFramework/ThemeHospitalDatabase.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\jonathan\documents\visual studio 2013\Projects\Theme Hospital\TH.UnitOfWorkEntityFramework\ThemeHospitalDatabase.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Jonathan\documents\visual studio 2013\Projects\Theme Hospital\TH.UnitOfWorkEntityFramework\ThemeHospitalDatabase.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
