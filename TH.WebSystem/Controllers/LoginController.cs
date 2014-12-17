978606ae3a1223641c051c22e44b8b9a840a4a14
Ste Prescott <mail@ste.me>
2014-12-17 20:26:05 +0000

    Login view done.

---
+++ TH.WebSystem/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TH.Container;
+using TH.Interfaces;
+using TH.WebSystem.Models;
+
+namespace TH.WebSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public bool AttemptLogin(LoginModel loginModel)
+        {
+            var username = loginModel.Username;
+            var password = loginModel.Password;
+
+            var staffMemberBusinessLogic = ThemeHospitalContainer.GetInstance<IStaffMemberBusinessLogic>();
+            var staffMember = staffMemberBusinessLogic.LoginStaffMember(loginModel.Username, loginModel.Password);
+            return staffMember != null ? true : false;
+        }
+    }
+}
