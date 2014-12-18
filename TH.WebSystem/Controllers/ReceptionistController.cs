c8c1138b89e58b84f92a8ccf7bb5dca552f7ca6d
Joe Fletcher <joe_pk@live.com>
2014-12-18 00:21:14 +0000

    Added scaffold for add patient

---
+++ TH.WebSystem/Controllers/ReceptionistController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TH.WebSystem.Models;
+
+namespace TH.WebSystem.Controllers
+{
+    public class ReceptionistController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddPatient()
+        {
+            return View(new AddPatientViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult AddPatient(AddPatientViewModel patient)
+        {
+            return RedirectToAction("Index", "Receptionist");
+        }
+    }
+}
---
+++ TH.WebSystem/Models/AddPatientViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Web;
+
+namespace TH.WebSystem.Models
+{
+    public class AddPatientViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Other Names")]
+        public string OtherNames { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required]
+        [Display(Name = "Contact Number")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Invalid Number")]
+        public string ContactNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Emergency Contact Name")]
+        public string EmergencyContactName { get; set; }
+
+        [Required]
+        [Display(Name = "Contact Number")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Invalid Number")]
+        public string EmergencyContactNumber { get; set; }
+    }
+}
